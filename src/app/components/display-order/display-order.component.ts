119287da82b3ed6c868ec6fa5dab5ce66897a5be
SnehalRajapurkar <snehalrajapurkar2000@gmail.com>
2022-10-07 16:36:09 +0530

    changed extension

--- src/app/components/display-order/display-order.component.ts
+++ src/app/components/display-order/display-order.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-display-order',
   templateUrl: './display-order.component.html',
-  styleUrls: ['./display-order.component.scss']
+  styleUrls: ['./display-order.component.css'],
 })
 export class DisplayOrderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/components/update-order/update-order.component.ts
+++ src/app/components/update-order/update-order.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-update-order',
   templateUrl: './update-order.component.html',
-  styleUrls: ['./update-order.component.scss']
+  styleUrls: ['./update-order.component.css'],
 })
 export class UpdateOrderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
