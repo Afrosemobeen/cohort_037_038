516015c50c2a7e170e14d6d3c02d57f9d0d291da
MrMukund <101634969+MrMukund@users.noreply.github.com>
2022-10-11 13:39:04 +0530

    sample commit 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,21 @@
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
-import { ReactiveFormsModule } from '@angular/forms';
-=======
 import { FormsModule } from '@angular/forms';
->>>>>>> fa1a8ab95445dcb6352ade5e7ae546bc1c95a84e
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { SignupFormComponent } from './components/signup-form/signup-form.component';
-
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [AppComponent, LoginFormComponent, SignupFormComponent],
-<<<<<<< HEAD
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
-=======
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
->>>>>>> fa1a8ab95445dcb6352ade5e7ae546bc1c95a84e
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
