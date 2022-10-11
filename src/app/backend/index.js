1bff9e7c54f573a71250a39d7b05f3fc07470625
Anchal Bathla <anchalbathla@microsoft.com>
2022-10-11 11:56:51 +0530

    create order operation created at backend

--- src/app/backend/index.js
+++ src/app/backend/index.js
@@ -9,7 +9,35 @@ app.use(express.json());
 //step 4- developing services
 app.get("/", function (req, res) {
   console.log(req);
-  res.send("Welcome to Purchase Order application");
+  res.send("Welcome to orders application");
+});
+
+app.get("/getallorders", function (req, res) {
+  console.log("I am from getallorders");
+  res.send(orderList);
+});
+
+
+app.post("/createorder", function (req, res) {
+
+  console.log("From create order service");
+  
+  // console.log(req);
+  const { body, params, query } = req; //object destructuring
+  console.log("body: ", body);
+  console.log("params: ", params);
+  console.log("query: ", query);
+  let orderNoList = orderList.map((order) => order.po_num);
+  let maxNo = 0;
+  if (orderNoList.length === 0) {
+    maxNo = 100;
+  } else {
+    maxNo = Math.max(...orderNoList);
+  }
+  body.po_num = maxNo + 1;
+  orderList = [...orderList, body];
+  console.log("new order saved successfully");
+  res.send(body);
 });
 
 //step 3- register aplication with port
@@ -20,8 +48,47 @@ app.listen(5000, function (err) {
     return;
   }
   if (!err) {
-    console.log(
-      "Purchase order Application started successfully and listening on port 5000"
-    );
+    console.log(" Purchase order Application started successfully and listening on port 5000");
   }
 });
+
+var orderList = [
+  {
+    po_num: 'po123',
+    date: '22 / 10 / 2011',
+    total_amt: 12000,
+    user_name: 'Snehal',
+    desc: 'I am snehal',
+    state: 'Karnataka',
+    city: 'Belgaum',
+  },
+  {
+    po_num: 'po456',
+    date: '21 / 15 / 2011',
+    total_amt: 12500,
+    user_name: 'Pranay',
+    desc: 'I am Pranay',
+    state: 'Maharashtra',
+    city: 'Mumbai',
+  },
+  {
+    po_num: 'po789',
+    date: '22 / 09 / 2022',
+    total_amt: 23000,
+    user_name: 'Ganesh',
+    desc: 'I am Ganesh',
+    state: 'Karnataka',
+    city: 'Dharwad',
+  },
+  {
+    po_num: 'po563',
+    date: '15 / 10 / 2016',
+    total_amt: 52000,
+    user_name: 'Sarthak',
+    desc: 'I am Sarthak',
+    state: 'Karnataka',
+    city: 'Belgaum',
+  },
+];
+
+
