04f140343f72150e6ab8e47cf240b63b32484c93
SnehalRajapurkar <snehalrajapurkar2000@gmail.com>
2022-10-13 22:36:27 +0530

    New folder structure setup

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -8,14 +8,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UsersComponent } from './components/user-management/users/users.component';
 import { EditUserComponent } from './components/user-management/edit-user/edit-user.component';
 import { DisplayUserComponent } from './components/user-management/display-user/display-user.component';
-import {FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MatIconModule} from '@angular/material/icon'
+import { MatIconModule } from '@angular/material/icon';
 import { HttpClientModule } from '@angular/common/http';
 import { UserOperationsComponent } from './components/user-management/user-operations/user-operations.component';
 import { OrderModule } from 'ngx-order-pipe';
 import { Ng2OrderModule } from 'ng2-order-pipe';
 import { AddUserComponent } from './components/user-management/add-user/add-user.component';
+import { SharedModule } from './shared/shared.module';
 @NgModule({
   declarations: [
     AppComponent,
@@ -35,7 +36,8 @@ import { AddUserComponent } from './components/user-management/add-user/add-user
     MatIconModule,
     MatDialogModule,
     BrowserAnimationsModule,
-    Ng2OrderModule
+    Ng2OrderModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
