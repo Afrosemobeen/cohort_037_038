3c96d638fef049561c26b9234debd07590727f54
Sujitha-T <216t.sujitha@gmail.com>
2022-10-14 19:55:33 +0530

    deleted user-management component

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,39 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { UsersComponent } from './components/user-management/users/users.component';
-import { EditUserComponent } from './components/user-management/edit-user/edit-user.component';
-import { DisplayUserComponent } from './components/user-management/display-user/display-user.component';
+
 import {FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatIconModule} from '@angular/material/icon'
+// import { MatDialogModule } from '@angular/material/dialog';
+// import { MatIconModule} from '@angular/material/icon'
 import { HttpClientModule } from '@angular/common/http';
-import { UserOperationsComponent } from './components/user-management/user-operations/user-operations.component';
-import { OrderModule } from 'ngx-order-pipe';
-import { Ng2OrderModule } from 'ng2-order-pipe';
-import { AddUserComponent } from './components/user-management/add-user/add-user.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent,
-    EditUserComponent,
-    DisplayUserComponent,
-    UserOperationsComponent,
-    AddUserComponent,
+   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule,
+    // NgbModule,
     ReactiveFormsModule,
     FormsModule,
     HttpClientModule,
-    MatIconModule,
-    MatDialogModule,
+    // MatIconModule,
+    // MatDialogModule,
     BrowserAnimationsModule,
-    Ng2OrderModule
+    // Ng2OrderModule
   ],
   providers: [],
   bootstrap: [AppComponent],
