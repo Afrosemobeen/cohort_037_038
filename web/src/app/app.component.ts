df86ec07342b754ebfe95a537fc7a83c7468ccb7
Sujitha-T <216t.sujitha@gmail.com>
2022-10-14 19:07:54 +0530

    Testing

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -3,11 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-<<<<<<< HEAD:src/app/app.component.ts
-  styleUrls: ['./app.component.scss'],
-=======
-  styleUrls: ['./app.component.css']
->>>>>>> 7c9981f8de3826fa78b869e8d032cdd2ac142966:web/src/app/app.component.ts
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'example2';
