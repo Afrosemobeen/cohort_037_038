1321f35867aa9e5e792190df169c80466b35070f
Sriganesh2000 <87692104+Sriganesh2000@users.noreply.github.com>
2022-10-14 12:16:34 +0530

    folder structure changed

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -5,26 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { UsersComponent } from './components/user-management/users/users.component';
-import { EditUserComponent } from './components/user-management/edit-user/edit-user.component';
-import { DisplayUserComponent } from './components/user-management/display-user/display-user.component';
+
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { HttpClientModule } from '@angular/common/http';
-import { UserOperationsComponent } from './components/user-management/user-operations/user-operations.component';
-import { OrderModule } from 'ngx-order-pipe';
+
+
 import { Ng2OrderModule } from 'ng2-order-pipe';
-import { AddUserComponent } from './components/user-management/add-user/add-user.component';
+
 import { SharedModule } from './shared/shared.module';
 @NgModule({
   declarations: [
-    AppComponent,
-    UsersComponent,
-    EditUserComponent,
-    DisplayUserComponent,
-    UserOperationsComponent,
-    AddUserComponent,
+    AppComponent
+
   ],
   imports: [
     BrowserModule,
