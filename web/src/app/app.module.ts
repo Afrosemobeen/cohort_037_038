cc36d8d570f076bc6a2da0e776ecc51b78b01436
Afrosemobeen <afrosemobeenmohammad@gmail.com>
2022-10-14 15:40:51 +0530

    deleted components

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -6,19 +6,14 @@ import { AppComponent } from './app.component';
 import { UserModuleModule } from "./user-module/user-module.module";
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CreateUserComponent } from './user-management/create-user/create-user.component';
-import { EditUserDetailsComponent } from './user-management/edit-user-details/edit-user-details.component'
 @NgModule({
   declarations: [
     AppComponent,
-    CreateUserComponent,
-    EditUserDetailsComponent,
    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    UserModuleModule,
     
   ],
   providers: [],
