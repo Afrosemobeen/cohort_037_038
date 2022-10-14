d2eeb01c9eb06fad2b8269bfb0b14364fc71442d
MrMukund <101634969+MrMukund@users.noreply.github.com>
2022-10-14 11:43:09 +0530

    sample commit 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
-
-import { ReactiveFormsModule } from '@angular/forms';
-=======
->>>>>>> cd28361f8bb34319f73a8d33340face0c7daf117
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
