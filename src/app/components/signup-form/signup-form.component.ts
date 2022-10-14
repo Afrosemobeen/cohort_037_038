311d1c711bbcb64a266395cf4dec1012b6d0ac04
MrMukund <101634969+MrMukund@users.noreply.github.com>
2022-10-14 16:41:30 +0530

    sample commit 3

--- src/app/components/signup-form/signup-form.component.ts
+++ src/app/components/signup-form/signup-form.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { HttpClient } from "@angular/common/http";
 @Component({
-  selector: 'app-signup-form',
-  templateUrl: './signup-form.component.html',
-  styleUrls: ['./signup-form.component.scss'],
+  selector: "app-signup-form",
+  templateUrl: "./signup-form.component.html",
+  styleUrls: ["./signup-form.component.scss"],
 })
 export class SignupFormComponent implements OnInit {
   signUpForm: FormGroup;
@@ -12,21 +12,21 @@ export class SignupFormComponent implements OnInit {
   constructor(private fb: FormBuilder, private http: HttpClient) {
     this.signUpForm = this.fb.group({
       username: [
-        '',
+        "",
         [
           Validators.required,
           Validators.email,
           Validators.pattern(
-            '[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,63}$'
+            "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,63}$"
           ),
         ],
       ],
       password: [
-        '',
+        "",
         [
           Validators.required,
           Validators.pattern(
-            '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,12}$'
+            "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,12}$"
           ),
         ],
       ],
