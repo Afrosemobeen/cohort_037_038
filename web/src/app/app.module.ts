b8f363c3967a8f8388caf62e44b72ce5f5f5380f
Dheeraj Patel <dheerajpatel24499@gmail.com>
2022-10-19 16:54:16 +0530

    previous commit

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,17 +1,44 @@
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UsersComponent } from './components/user-management/users/users.component';
+import { EditUserComponent } from './components/user-management/edit-user/edit-user.component';
+import { DisplayUserComponent } from './components/user-management/display-user/display-user.component';
+import {FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule} from '@angular/material/icon'
+import { HttpClientModule } from '@angular/common/http';
+import { UserOperationsComponent } from './components/user-management/user-operations/user-operations.component';
+import { OrderModule } from 'ngx-order-pipe';
+import { Ng2OrderModule } from 'ng2-order-pipe';
+import { AddUserComponent } from './components/user-management/add-user/add-user.component';
 @NgModule({
   declarations: [
     AppComponent,
+    AddUserComponent,
+    UsersComponent,
+    EditUserComponent,
+    DisplayUserComponent,
+    UserOperationsComponent,
+    
   ],
   imports: [
+    BrowserModule,
     AppRoutingModule,
-    BrowserModule
+    NgbModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    MatIconModule,
+    MatDialogModule,
+    BrowserAnimationsModule,
+    Ng2OrderModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
