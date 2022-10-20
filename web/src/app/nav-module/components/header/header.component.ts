c0597b73dfee0b99c459a146e1a80627fb59c8d7
Dheeraj Patel <dheerajpatel24499@gmail.com>
2022-10-20 15:32:20 +0530

    Dynamic Heading,header ts updated

--- web/src/app/nav-module/components/header/header.component.ts
+++ web/src/app/nav-module/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NavServiceService} from 'app/nav-module/services/nav-service.service'
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-pagetitle:any;
-  constructor() { }
-
-  ngOnInit(): void {
+  pagetitle: String ="";
+  public serNav:any=[];
+  constructor( private navService:NavServiceService) {}
+   
+    ngOnInit(): void {
+  
+  const data = this.navService.sendDataToHeader();
+    data.subscribe({
+      next: (_data:any)=>{
+          //console.log("elsfpdkfpoesop")
+          this.navService.getNavFromDatabase()
+    .subscribe({
+      next: (data:any)=>{
+        // console.log("Data:",data);
+        this.serNav = structuredClone(data);
+        // this.sendnavhardcopy = structuredClone(this.serNav);
+        this.navService .sendInitialDataToService(data);
+        //console.log("rigjiofvafdkfpldskvdlvldddddddddddddddddddddddd")
+      },
+      error: ()=>{},
+      complete: ()=>{},
+    })
+  
+      }, error: (err:any)=>{
+        console.log(err);
+      }
+    }) ;
+  
+    }
+  
+  pageHeading(pagetitle:String){
+  this.pagetitle = pagetitle;
   }
- 
-}
+  
+  }
