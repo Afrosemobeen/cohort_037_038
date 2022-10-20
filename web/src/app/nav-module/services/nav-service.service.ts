785ccb8e9f6af81a8d9246a90573a08fa28dedcb
namitrathod <namitrathod07@gmail.com>
2022-10-20 16:37:02 +0530

    added codes to services for api call

--- web/src/app/nav-module/services/nav-service.service.ts
+++ web/src/app/nav-module/services/nav-service.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NavServiceService {
+ private myMethodSubject = new BehaviorSubject<any>(""); 
+  constructor(private http: HttpClient) { }
+  
+// for backend part
+  getNavFromDatabase(){
+        return this.http.get("http://localhost:5000/navlist");
+  }
+      saveNavlistFromNavedit(navTodisplay:any){
+        return this.http.put("http://localhost:5000/navedit", navTodisplay);
+  }
 
-  constructor() { }
+
 }
