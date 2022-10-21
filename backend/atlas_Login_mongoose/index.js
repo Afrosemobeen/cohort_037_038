e4bfa36b8b6f55a841b74e84c5cfdc330b98df51
Sujitha-T <216t.sujitha@gmail.com>
2022-10-21 17:24:44 +0530

    backend index.js updated

---
+++ backend/atlas_Login_mongoose/index.js
@@ -0,0 +1,22 @@
+var express = require ('express');
+var cors = require('cors');
+var mongoose = require('./db/db');
+
+var app = express();
+app.use(cors())
+app.use(express.json());
+app.get("/", function(req, res){
+    console.log(req);
+    res.send("Welcome to Employee Operations App");
+})
+app.use(require('./routes/emp_routes'));
+app.listen(5000, function (err){
+    if(err){
+        console.log("Got error while listening on port some changes");
+        console.log(err);
+        return;
+    }
+    if(!err){
+        console.log("Application is started successfully and listening on port: 5000...");
+    }
+})
