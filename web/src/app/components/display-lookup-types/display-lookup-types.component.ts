771e82e80a3d59fcfb94f80e83cbeb62f7d9b080
moulikori <109732285+moulikori@users.noreply.github.com>
2022-10-21 12:25:50 +0530

    createing items

--- web/src/app/components/display-lookup-types/display-lookup-types.component.ts
+++ web/src/app/components/display-lookup-types/display-lookup-types.component.ts
@@ -22,5 +22,59 @@ export class DisplayLookupTypesComponent implements OnInit {
     console.log("lookupInfo:", this.lookupInfo);
   }
   
-  
+  createLookup(){
+    const lookupType= this.manageSer.lookupInfo.lookupType
+    console.log("lookuptype", lookupType);
+    this.manageSer.getLookupsByType(lookupType)
+    .subscribe({
+      next: (result:any)=>{
+        console.log("result:", result);
+        console.log("newLookupValue",this.newLookupValue);
+
+       let filteredLookups = result.filter((lookup: any)=>{
+          return this.lookupInfo.lookupType ===  lookup.lookup_type && lookup.lookup_value === this.newLookupValue
+        })
+        console.log("filteredLookups", filteredLookups);
+        
+       if(filteredLookups.length > 0){
+        alert("duplicate value");
+       }
+       else{
+        let newLookupObj = {
+          status: "active",
+          lookup_type: this.lookupInfo.lookupType,
+          lookup_value: this.newLookupValue
+        }
+        
+        this.manageSer.createLookup(newLookupObj).subscribe({
+        next: (result) => {
+          console.log('createresult:', result);
+          this.clearForm();
+          this.manageSer.getLookupsByType(lookupType).subscribe({
+            next: (data: any) => {
+              console.log('data:', data);
+              let newLookupInfo ={...this.lookupInfo, lookupTypeItems: data}
+              this.onChangeLookup.emit(newLookupInfo)
+            },
+            error: () => {},
+            complete: () => {},
+          });
+        },
+        error: () => {},
+        complete: () => {},
+      });
+        
+       }
+      },
+        error: ()=>{
+          alert("Something went wrong");
+        },
+        complete: ()=>{}
+      })
+  }
+
+  clearForm() {
+      this.newLookupValue = '';
+    }
+
 }
