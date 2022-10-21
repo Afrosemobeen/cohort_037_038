50e52c198c432c7b536ee5287e998f2a3d3c8988
Sujitha-T <216t.sujitha@gmail.com>
2022-10-21 11:25:08 +0530

    API call in service

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {FormsModule, ReactiveFormsModule } from '@angular/forms';
-// import { MatDialogModule } from '@angular/material/dialog';
-// import { MatIconModule} from '@angular/material/icon'
+
 import { HttpClientModule } from '@angular/common/http';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { SignupFormComponent } from './components/signup-form/signup-form.component';
@@ -24,14 +23,11 @@ import { SignupFormComponent } from './components/signup-form/signup-form.compon
   imports: [
     BrowserModule,
     AppRoutingModule,
-    // NgbModule,
     ReactiveFormsModule,
     FormsModule,
     HttpClientModule,
-    // MatIconModule,
-    // MatDialogModule,
-    BrowserAnimationsModule,
-    // Ng2OrderModule
+    BrowserAnimationsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent],
