44c373ba60b884a0a5c7908e4f4075451a7eea73
DeepakChouksey <dchouksey56@gmail.com>
2022-10-21 16:01:12 +0530

    SignUp-TS Commit

--- web/src/app/components/login-form/login-form.component.ts
+++ web/src/app/components/login-form/login-form.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
-import { LoginService } from 'src/app/services/login.service';
+import { LoginService } from 'app/services/login.service';
+
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
--- web/src/app/components/signup-form/signup-form.component.ts
+++ web/src/app/components/signup-form/signup-form.component.ts
@@ -1,25 +1,57 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { LoginService } from 'app/services/login.service';
 @Component({
   selector: 'app-signup-form',
   templateUrl: './signup-form.component.html',
   styleUrls: ['./signup-form.component.scss']
 })
 export class SignupFormComponent implements OnInit {
-
+  
   signupForm: FormGroup;
-  constructor(private fb: FormBuilder) {
+  submitted = false;
+  constructor(private loginSer: LoginService,private fb: FormBuilder, private http: HttpClient) { 
     this.signupForm =  this.fb.group({
-      username: [''],
-      password: [''],
+      username: ['', [Validators.required,Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
      
     }
     );
-   }
+  }
 
   ngOnInit(): void {
   }
-signUp(){
-
-}
+  signUp(){
+    // console.log(this.signupForm.value);
+    this.submitted = true;
+    if(!this.signupForm.controls['username'].value || !this.signupForm.controls['password'].value){
+      alert("data is missing for some fields");
+      return;
+    }
+    const user = {
+      ...this.signupForm.value
+    }
+    console.log("user",user);
+    this.loginSer.createUser(user)
+    .subscribe({
+      next: (result)=>{
+      //   if (result.find(x => x.username === user.username)) {
+      //     alert("Username already exists");
+      // }
+      // const user = result.find((a:any)=>{
+      //   if(a.username === this.signupForm.value.email) {
+      //     alert("Username already exists");
+      //   }
+      // })
+        console.log("result:", result);
+        alert("sign up successful");
+        this.signupForm.reset();
+      },
+        error: ()=>{
+          alert("Something went wrong");
+        },
+        complete: ()=>{}
+      })
+  }
 }
