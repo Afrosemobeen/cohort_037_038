3fe5bb75e9ee3d4588d10d3e8a1c36c8f6af1774
SnehalRajapurkar <snehalrajapurkar2000@gmail.com>
2022-10-25 15:22:19 +0530

    Changed input type

--- backend/models/order.js
+++ backend/models/order.js
@@ -5,7 +5,7 @@ var orderSchema = new mongoose.Schema(
   {
     po_num: String,
     date: String,
-    total_amt: String,
+    total_amt: Number,
     username: String,
     desc: String,
     state: String,
