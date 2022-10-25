867fe368381745adaafc64730f97904a5689133d
Sujitha-T <216t.sujitha@gmail.com>
2022-10-25 11:03:18 +0530

    Service updated with post method for login

--- web/src/app/services/login.service.ts
+++ web/src/app/services/login.service.ts
@@ -1,15 +1,21 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-
+  signupForm: any=[];
   constructor(private http: HttpClient) { }
   getUser(){
     return   this.http.get("http://localhost:5000/users")
   }
   createUser(user: any){
-    return  this.http.post<any>("http://localhost:5000/users", user)
+    console.log("value: ", this.signupForm);
+    return  this.http.post("http://localhost:5000/users", user)
+  }
+  checkLogin(user: any){
+    console.log("user value: ", user);
+    return  this.http.post("http://localhost:5000/users/login", user)
   }
 }
