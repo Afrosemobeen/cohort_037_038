d76a2f141ca9578be6f2ff041680dc96294a6ccb
janhavi <janhavi6701@gmail.com>
2022-10-25 12:38:22 +0530

    Login Routes

---
+++ backend/atlas_Login_mongoose/routes/login_routes.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var empOperations = require("../controllers/emp_controller");
+
+var router = express.Router();
+
+router.get("/users", empOperations.getUser);
+
+router.post("/users", empOperations.createUser);
+
+module.exports = router;
