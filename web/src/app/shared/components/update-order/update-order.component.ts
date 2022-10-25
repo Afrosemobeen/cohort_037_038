7456c819275951115c4e542ea1eb193ba92c9e61
SnehalRajapurkar <snehalrajapurkar2000@gmail.com>
2022-10-25 18:25:34 +0530

    unique ponumber applied to update component

--- web/src/app/shared/components/update-order/update-order.component.ts
+++ web/src/app/shared/components/update-order/update-order.component.ts
@@ -22,6 +22,8 @@ export class UpdateOrderComponent implements OnInit {
   cities: any = [];
   poNumbers: any = [];
   orderForm = this.purchaseOrderSer.getOrderFormData();
+  id: any;
+  flag: boolean = false;
 
   constructor(public purchaseOrderSer: PurchaseOrderService) {
     this.onCancelUpdateEvent = new EventEmitter();
@@ -30,22 +32,22 @@ export class UpdateOrderComponent implements OnInit {
   ngOnInit(): void {
     this.orderForm.setValue({ ...this.order });
     this.states = this.purchaseOrderSer.getStates().subscribe((data: any) => {
-      console.log(data);
       this.purchaseOrderSer.states = data;
       this.states = data;
     });
     this.cities = this.purchaseOrderSer.getCities().subscribe((data: any) => {
-      console.log(data);
       this.purchaseOrderSer.cities = data;
       this.cities = data;
     });
     this.poNumbers = this.purchaseOrderSer.getPoNumber().subscribe((data) => {
-      console.log(data);
       this.purchaseOrderSer.poNumbers = data;
       this.poNumbers = data;
     });
   }
 
+  selectedPo(e: any) {
+    this.id = e.target.value;
+  }
   ngOnChanges(changes: SimpleChanges) {
     console.log('Changes: ', changes);
     console.log('Changes: ', changes['order'].currentValue);
@@ -53,24 +55,33 @@ export class UpdateOrderComponent implements OnInit {
   }
 
   updateOrder() {
-    console.log('order: ', this.orderForm.value);
-    this.purchaseOrderSer.updateOrder(this.orderForm.value).subscribe({
-      next: (result: any) => {
-        console.log('Result: ', result);
+    const found = this.purchaseOrderSer.orderList.some(
+      (el: any) => el.po_num === this.id
+    );
+    if (!found) {
+      this.flag = false;
+      console.log('order: ', this.orderForm.value);
+      this.purchaseOrderSer.updateOrder(this.orderForm.value).subscribe({
+        next: (result: any) => {
+          console.log('Result: ', result);
 
-        this.purchaseOrderSer.getAllOrders().subscribe({
-          next: (data: any) => {
-            console.log('data', data);
-            this.purchaseOrderSer.orderList = data;
-            this.cancelUpdateForm();
-          },
-          error: () => {},
-          complete: () => {},
-        });
-      },
-      error: () => {},
-      complete: () => {},
-    });
+          this.purchaseOrderSer.getAllOrders().subscribe({
+            next: (data: any) => {
+              console.log('data', data);
+              this.purchaseOrderSer.orderList = data;
+              this.cancelUpdateForm();
+            },
+            error: () => {},
+            complete: () => {},
+          });
+        },
+        error: () => {},
+        complete: () => {},
+      });
+    } else {
+      this.flag = true;
+      console.log('Ponumber exists');
+    }
   }
 
   cancelUpdateForm() {
